2457b3add3a5c9b00a89d19fa8d5c0e3bdbf4c37
Lars Michaelis <mail@larmic.de>
2016-09-27 10:58:53 +0200

    add some logging output

--- src/main/java/de/larmic/butterfaces/joinfaces/security/Identity.java
+++ src/main/java/de/larmic/butterfaces/joinfaces/security/Identity.java
@@ -1,5 +1,7 @@
 package de.larmic.butterfaces.joinfaces.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
@@ -11,18 +13,31 @@ import org.springframework.stereotype.Component;
 @Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class Identity {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     private String userName;
     private boolean loggedIn;
 
     public String login(String userName, String password) {
+        log.info("Try to login user {}", userName);
         this.loggedIn = "admin".equals(userName) && "admin".equals(password);
-        this.userName = loggedIn ? userName : null;
-        return loggedIn ? "/secure/index.jsf?faces-redirect=true" : null;
+
+        if (loggedIn) {
+            this.userName = userName;
+            log.info("User {} logged in.", userName);
+            return "/secure/index.jsf?faces-redirect=true";
+        } else {
+            this.userName = null;
+            log.info("User {} not logged in. Bad credentials.", userName);
+            return null;
+        }
     }
 
     public String logout() {
         loggedIn = false;
         userName = null;
+
+        log.info("User {} logged out", userName);
 
         return "/login.jsf?faces-redirect=true";
     }
